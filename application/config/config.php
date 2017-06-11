c22a1fdd7cf7482346929c44708207db2d672d52
pyokizzz <rismanefendi007@gmail.com>
2017-06-11 15:47:40 +0700

    Create config.php

---
+++ application/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+define('ENVIRONMENT', 'development');
+
+if (ENVIRONMENT == 'development' || ENVIRONMENT == 'dev') {
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+}
+
+define('URL_PUBLIC_FOLDER', 'public');
+define('URL_PROTOCOL', '//');
+define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
+define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])));
+define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'mini');
+define('DB_USER', 'root');
+define('DB_PASS', '12345678');
+define('DB_CHARSET', 'utf8');
